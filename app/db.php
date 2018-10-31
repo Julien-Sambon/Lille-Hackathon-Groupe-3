962b97b45a102752fa35fb12b230cf9d5acf65b2
Sami Aid <samiaid2@gmail.com>
2018-10-31 09:48:58 +0100

    update db onnect

--- app/db.php
+++ app/db.php
@@ -1,5 +1,5 @@
 <?php
-define('APP_DB_USER', 'kuchkz');
-define('APP_DB_PWD', 'Kuchkz_59');
+define('APP_DB_USER', 'root');
+define('APP_DB_PWD', 'Lol456852/');
 define('APP_DB_HOST', 'localhost');
 define('APP_DB_NAME', 'db_hackathon');
