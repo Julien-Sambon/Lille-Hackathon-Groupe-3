872e4602b32e55ff0bda8c615dcfd53f7fbf28f0
Sambon Julien <steeper@live.fr>
2018-10-31 11:47:16 +0100

    Add justify content center on row Form.

--- app/db.php
+++ app/db.php
@@ -1,5 +1,5 @@
 <?php
-define('APP_DB_USER', '');
-define('APP_DB_PWD', '');
+define('APP_DB_USER', 'julien');
+define('APP_DB_PWD', '$C6b7722a00');
 define('APP_DB_HOST', 'localhost');
 define('APP_DB_NAME', 'db_hackathon');
