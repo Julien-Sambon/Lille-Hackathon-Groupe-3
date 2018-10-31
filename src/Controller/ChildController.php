56003ff5afc53ce2ef4463657d7c5f3c28451345
Christophe Kuchmac <christophekuchmac@free.fr>
2018-10-31 11:54:43 +0100

    View product.

--- src/Controller/ChildController.php
+++ src/Controller/ChildController.php
@@ -59,7 +59,7 @@ class ChildController extends AbstractController
         $json_data = json_decode($json_source);
         $product = $json_data->product;
 
-        echo "Nom du produit : $product->product_name_fr" . "<br />";
+       /* echo "Nom du produit : $product->product_name_fr" . "<br />";
         echo "Id du produit : $product->_id"  . "<br />";
         echo "Marque : $product->brands" . "<br />";
         echo "Quantité : $product->quantity" . "<br />";
@@ -68,8 +68,17 @@ class ChildController extends AbstractController
         echo "Pays de vente : " . $product->countries . "<br />";
         echo "Liste des ingrédients : " . $product->ingredients_text_debug . "<br />";
         echo "Traces éventuelles : " . $product->traces_from_user . "<br />";
-        echo "<img src='$product->image_front_url'>" . "<br />";
-       die();
+        echo "<img src='$product->image_front_url'>" . "<br />";*/
+
+        return $this->twig->render('Child/selectinventory.html.twig', [
+            'name' => $product->product_name_fr,
+            'brand' => $product->brands,
+            'quantity' => $product->quantity,
+            'packaging' => $product->packaging_tags['0'],
+            'countries' => $product->countries,
+            'ingredients' => $product->ingredients_text_debug,
+            'image' => $product->image_front_url
+        ]);
     }
 
     public function giveRandomCandies(): array
